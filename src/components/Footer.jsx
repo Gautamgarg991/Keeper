344358d592a04eb9abf9ebf5b296d838e6259ca0
gautamgarg991 <gautam.garg@elucidata.io>
2022-11-22 20:48:15 +0530

    Initial

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+function Footer(){
+    const currYear = new Date().getFullYear();
+    return <footer>
+        Copyright © {currYear}
+    </footer>
+}
+export default Footer;
