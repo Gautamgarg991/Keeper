a2f4c7aea6e95b310fe662a2e3ba56008e628437
gautamgarg991 <gautam.garg@elucidata.io>
2023-01-02 11:40:44 +0530

    Updated

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,18 @@ import React from "react";
 import Header from "./Header.jsx"
 import Footer from "./Footer.jsx"
 import Note from "./Note.jsx"
+import notes from "../notes"
+
 
 function App(){
     return <div>
         <Header />
-        <Note />
+        {notes.map( (noteItem) =>(
+                 <Note key={noteItem.key} 
+                title={noteItem.title} 
+                description={noteItem.description}/>
+                )
+            )}
         <Footer />
     </div>
 }
---
+++ src/notes.js
@@ -0,0 +1,23 @@
+const notes=[
+    {
+        key:1,
+        title:"Data Structure",
+        description:"Complete the Trees before the Interview"
+    },
+    {
+        key:2,
+        title:"Grocery",
+        description:"Salt, Shop, Shampoo"
+    },
+    {
+        key:3,
+        title:"Places to Visit",
+        description:"CHimachal, Uttrakhand, Mumbai"
+    },
+    {
+        key:4,
+        title:"Places to Visit",
+        description:"CHimachal, Uttrakhand, Mumbai"
+    }
+];
+export default notes;
